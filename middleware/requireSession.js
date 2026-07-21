82d1f1b87767f56d8c93df7e73182ea78247b96a
Edward <cololacalempira5@gmail.com>
2026-07-20 20:22:13 -0600

    Updated beautiful Kintsugi CSS theme

---
+++ middleware/requireSession.js
@@ -0,0 +1,11 @@
+function requireSession(req, res, next) {
+
+    if (req.session && req.session.user) {
+        return next();
+    }
+
+    return res.redirect('/login');
+
+}
+
+module.exports = requireSession;
